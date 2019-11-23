643a5105e93ebaeee12e1e130c030f9da641701b
SamuelB2020 <12758689+SamuelB2020@users.noreply.github.com>
2019-11-23 19:42:50 +1100

    Revert "Added Index.js and test script"
    Added Index.js and test script

---
+++ index.js
@@ -0,0 +1,129 @@
+module.exports = class Angle{
+    constructor (angle){
+        if(typeof angle == "string"){
+            angle = stringDef(angle);
+        }
+        else{
+        angle = parseFloat(angle);
+        }   
+        this.degrees = angle;
+        this.radians = angle*(Math.PI/180);
+        this.turns = angle/360;
+        this.gons = angle*(10/9);
+    }
+    principleAngle(){
+        var temp = this.degrees;
+        while(temp > 180 || temp < -180){
+            if(temp > 180){
+                temp -= 360;
+            }
+            else{
+                temp += 360;
+            }
+        }
+        return new Angle(temp);
+        
+    }
+    Normalise(){
+        var temp = this.degrees;
+        while(temp < 0 || temp > 360){
+            if(temp > 360){
+                temp -= 360;
+            }
+            else{
+                temp += 360;
+            }
+        }
+        return new Angle(temp);
+    }
+    cos(){
+        return Math.cos(this.radians);
+    }
+    sin(){
+        return Math.sin(this.radians);
+    }
+    tan(){
+        return Math.tan(this.radians);
+    }
+    quadrant(){
+        var Normalised = this.Normalise().degrees;
+        if(Normalised%90 == 0){
+            return -1;
+        }
+        if(Normalised > 0 && Normalised < 90){
+            return 1;
+        }
+        if(Normalised > 90 && Normalised < 180){
+            return 2;
+        }
+        if(Normalised > 180 && Normalised < 270){
+            return 3;
+        }
+        if(Normalised > 270 && Normalised < 360){
+            return 4;
+        }
+    }
+    cardinalDirection(){
+        switch (this.Normalise().degrees) {
+            case 0:
+                return "N";
+            case 360:
+                return "N";
+            case 90:
+                 return "E";     
+            case 180:
+                return "S";
+             case 270:
+                return "W"
+            default:
+                return -1;
+        }
+    }
+    IntercardinalDirection(){
+        switch (this.Normalise().degrees) {
+            case 0:
+                return "N";
+            case 360:
+                return "N";
+            case 45:
+                return "NE"
+            case 90:
+                 return "E"; 
+            case 135:
+                return "SE";
+            case 180:
+                return "S";
+            case 225: 
+                return "SW";
+            case 270:
+                return "W";
+            case 315:
+                return "NW";
+            default:
+                return -1;
+        }
+    }
+    between(a,b){
+        var normalA = a.Normalise();
+        var normalB = b.Normalise();
+        return this.degrees >= Math.min(normalA.degrees,normalB.degrees) && this.degrees <=Math.max(normalA.degrees, normalB.degrees);
+    }
+}
+function stringDef(string){
+    if(string.indexOf('d') != -1){
+        return parseFloat(string.split('d')[0]);
+    }
+    else if(string.indexOf('r') != -1){
+        var rad = parseFloat(string.split('r')[0]);
+        return rad*(180/Math.PI);
+    }
+    else if(string.indexOf("t") != -1){    
+        return parseFloat(string.split("t")[0])*360;
+    }
+    else if(string.indexOf("g") != -1){
+        return parseFloat(string.split("g")[0])*(9/10);
+    }
+    else{
+        return 0;
+    }
+}
---
+++ tests/test.js
@@ -0,0 +1,5 @@
+const Angle = require('../index');
+var p2 = Math.PI/8
+var data = new Angle("1.25t");
+console.log(data);
+
